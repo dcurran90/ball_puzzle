315c1339ffcae99afea051bed0691e1f20932b81
Michael Main <mike.main@rackspace.com>
2012-02-01 21:39:39 -0600

    Finished

--- grails-app/services/com/rackspace/puzzle/Ball.groovy
+++ grails-app/services/com/rackspace/puzzle/Ball.groovy
@@ -1,15 +1,37 @@
 package com.rackspace.Puzzle
 
+/**
+ * @author mich4570
+ * 
+ * Ball
+ * 
+ * Represents a weighted ball, not to be confused with an Aperture Science Weighted Storage Cube.
+ *
+ */
 class Ball 
 {
 	def weight
 	static curr_id = 0
 	def id
-	def Ball(def weight = 1.0)
+	
+	/**
+	 * Constructor
+	 * 
+	 * @param weight
+	 * @return void
+	 */
+	def Ball(weight = 1.0)
 	{
 		id = ++curr_id
 		this.weight = weight
 	}
+	
+	/**
+	 * ResetID
+	 * 
+	 * Resets the static id variable to 0
+	 * @return void
+	 */
 	static def ResetID()
 	{
 		curr_id = 0
--- grails-app/services/com/rackspace/puzzle/Scale.groovy
+++ grails-app/services/com/rackspace/puzzle/Scale.groovy
@@ -1,21 +1,55 @@
 package com.rackspace.Puzzle
 import com.rackspace.Puzzle.Ball
 
+/**
+ * @author mich4570
+ * 
+ * Scale
+ * 
+ * Represents a virtual scale with two plates
+ *
+ */
 class Scale {
 	def leftWeight = 0
 	def rightWeight = 0
+	
+	/**
+	 * Adds a ball to the left plate
+	 * 
+	 * @param ball
+	 * @return void
+	 */
 	def addBallLeft(ball)
 	{
 		leftWeight += ball.weight
 	}
+	
+	/**
+	 * Adds a ball to the right plate
+	 * 
+	 * @param ball
+	 * @return void
+	 */
 	def addBallRight(ball)
 	{
 		rightWeight += ball.weight
 	}
+	
+	/**
+	 * Resets both plates to zero weight
+	 * 
+	 * @return void
+	 */
 	def reset()
 	{
 		leftWeight = rightWeight = 0
 	}
+	
+	/**
+	 * Compares the weight on both plates and returns -1 if the left plate 
+	 * is heavier, 1 if the right plate is heavier, and 0 if they are equal
+	 * @return integer
+	 */
 	def compare()
 	{
 		println rightWeight + " versus " + leftWeight
